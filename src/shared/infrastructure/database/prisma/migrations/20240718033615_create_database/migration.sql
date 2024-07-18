ee9de7dc69e5ca83ce0d98e240afc240630e7923
Italo Souza Melo <italosm29@gmail.com>
2024-07-18 00:36:35 -0300

    database att

--- src/shared/infrastructure/database/prisma/migrations/20240718033615_create_database/migration.sql
+++ src/shared/infrastructure/database/prisma/migrations/20240718033615_create_database/migration.sql
@@ -227,6 +227,35 @@ CREATE TABLE "media_types" (
     CONSTRAINT "media_types_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "appointments" (
+    "id" UUID NOT NULL,
+    "customer_id" UUID NOT NULL,
+    "store_id" UUID NOT NULL,
+    "retail" DECIMAL(10,2) NOT NULL,
+    "wholesale" DECIMAL(10,2),
+    "is_wholesale" BOOLEAN NOT NULL DEFAULT false,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "payment_method" "PaymentType" NOT NULL DEFAULT 'CASH',
+    "expiration_date" DATE,
+    "note" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" DATE,
+
+    CONSTRAINT "appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointment_service" (
+    "id" UUID NOT NULL,
+    "appointment_id" UUID NOT NULL,
+    "service_id" UUID NOT NULL,
+    "store_id" UUID NOT NULL,
+
+    CONSTRAINT "appointment_service_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -299,6 +328,24 @@ CREATE INDEX "media_entity_type_entity_id_idx" ON "media"("entity_type", "entity
 -- CreateIndex
 CREATE UNIQUE INDEX "media_types_store_id_name_key" ON "media_types"("store_id", "name");
 
+-- CreateIndex
+CREATE INDEX "appointments_store_id_customer_id_idx" ON "appointments"("store_id", "customer_id");
+
+-- CreateIndex
+CREATE INDEX "appointments_store_id_idx" ON "appointments"("store_id");
+
+-- CreateIndex
+CREATE INDEX "appointments_customer_id_idx" ON "appointments"("customer_id");
+
+-- CreateIndex
+CREATE INDEX "appointment_service_appointment_id_idx" ON "appointment_service"("appointment_id");
+
+-- CreateIndex
+CREATE INDEX "appointment_service_service_id_idx" ON "appointment_service"("service_id");
+
+-- CreateIndex
+CREATE INDEX "appointment_service_store_id_idx" ON "appointment_service"("store_id");
+
 -- AddForeignKey
 ALTER TABLE "users_tokens" ADD CONSTRAINT "users_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -346,3 +393,18 @@ ALTER TABLE "media" ADD CONSTRAINT "media_media_type_id_fkey" FOREIGN KEY ("medi
 
 -- AddForeignKey
 ALTER TABLE "media_types" ADD CONSTRAINT "media_types_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointment_service" ADD CONSTRAINT "appointment_service_appointment_id_fkey" FOREIGN KEY ("appointment_id") REFERENCES "appointments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointment_service" ADD CONSTRAINT "appointment_service_service_id_fkey" FOREIGN KEY ("service_id") REFERENCES "services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointment_service" ADD CONSTRAINT "appointment_service_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
